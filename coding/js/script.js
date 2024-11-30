038531305d24750c0d4efdbefbdeab506f85d633
오민석 <dhalstjr03@gmail.com>
2024-11-30 19:16:03 +0900

    시리즈 섹션까지 완성

--- coding/js/script.js
+++ coding/js/script.js
@@ -29,4 +29,122 @@ $(function () {
     },
     "<"
   );
+
+  /* 탭메뉴 활성 */
+  const $tabMenu = $(".tab-menu > a");
+  const $tabContent = $(".tab-con > div");
+
+  //   console.log($tabMenu, $tabContent);
+
+  $tabContent.hide().eq(0).show();
+
+  $tabMenu.on("click", function (e) {
+    e.preventDefault();
+
+    const tIdx = $(this).index();
+
+    // console.log(tIdx);
+
+    $tabMenu.removeClass("on").eq(tIdx).addClass("on");
+    $tabContent.hide().eq(tIdx).show();
+  });
+
+  const $chaSlider = new Swiper(".testSlider1", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    // autoplay: true,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: ".btn.btn-next",
+      prevEl: ".btn.btn-prev",
+    },
+
+    observer: true,
+    observerParents: true,
+  });
+
+  //   동일한 형태의 슬라이드의 경우, 공통의 이름으로 한 번에 설정 적용
+  const $testSLider2 = new Swiper(".testSlider", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    observer: true,
+    observeParents: true,
+  });
+
+  /* 명대사 섹션 */
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+    trigger: "movie-famousline",
+    start: "top top",
+    end: "10%",
+    scrub: true,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      document.querySelector(
+        ".movie-famousline"
+      ).style.background = `rgba(0,0,0 ${progress})`;
+    },
+  });
+
+  //   배경색상전환
+  ScrollTrigger.create({
+    trigger: ".movie-famousline",
+    start: "10%",
+    end: "bottom bottom",
+    markers: true,
+    scrub: true,
+
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const startColor = { r: 0, g: 0, b: 0 };
+      const endColor = { r: 74, g: 144, b: 226 };
+
+      const currentColor = {
+        r: Math.round(startColor.r + (endColor.r - startColor.r) * progress),
+        g: Math.round(startColor.g + (endColor.g - startColor.g) * progress),
+        b: Math.round(startColor.b + (endColor.b - startColor.b) * progress),
+      };
+
+      document.querySelector(
+        ".movie-famousline"
+      ).style.background = `rgb(${currentColor.r}, ${currentColor.g},${currentColor.b})`;
+    },
+  });
+
+  const lines = document.querySelectorAll(".quote-line");
+  lines.forEach((line, index) => {
+    gsap.fromTo(
+      line,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".quote-con",
+          start: "top 80%",
+          end: "top 20%",
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  /*   const teardrop = document.querySelector(".tear");
+  gsap.to(teardrop, {
+    y: 800,
+    duration: 2,
+    repeat: -1,
+    ease: "power1.in",
+  }); */
 });
